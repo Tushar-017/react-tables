ebfe164747d70d61eed6050ef9ba8434074cb478
Tushar Rajput <tusharo17rajput@gmail.com>
2023-01-18 11:49:04 +0530

    basic table with header

---
+++ src/component/columns.js
@@ -0,0 +1,30 @@
+export const COLUMNS = [
+  {
+    Header: "Id",
+    accessor: "id",
+  },
+  {
+    Header: "First Name",
+    accessor: "first_name",
+  },
+  {
+    Header: "Last Name",
+    accessor: "last_name",
+  },
+  {
+    Header: "Email",
+    accessor: "email",
+  },
+  {
+    Header: "DOB",
+    accessor: "date_of_birth",
+  },
+  {
+    Header: "Age",
+    accessor: "age",
+  },
+  {
+    Header: "Phone",
+    accessor: "phone",
+  },
+]
