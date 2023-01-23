fa580fb13a0afac84c458cb1b40e5c76393504b1
Tushar Rajput <tusharo17rajput@gmail.com>
2023-01-23 11:55:10 +0530

    paggination in fetched table

--- src/component/FetchingTable.jsx
+++ src/component/FetchingTable.jsx
@@ -1,14 +1,30 @@
 import {
+  ArrowLeftIcon,
+  ArrowRightIcon,
+  ChevronLeftIcon,
+  ChevronRightIcon,
+} from "@chakra-ui/icons"
+import {
   Button,
   Center,
+  Flex,
   Heading,
+  IconButton,
   Image,
+  NumberDecrementStepper,
+  NumberIncrementStepper,
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  Select,
   Spinner,
   Table,
   Tbody,
   Td,
+  Text,
   Th,
   Thead,
+  Tooltip,
   Tr,
 } from "@chakra-ui/react"
 import axios from "axios"
@@ -16,7 +32,7 @@ import React from "react"
 import { useEffect } from "react"
 import { useMemo } from "react"
 import { useState } from "react"
-import { useTable, useSortBy } from "react-table"
+import { useTable, useSortBy, usePagination } from "react-table"
 
 const URL = "https://fakestoreapi.com/products"
 
@@ -68,14 +84,28 @@ const FetchingTable = () => {
   const [products, setProducts] = useState([])
   const columns = useMemo(() => tableColumn, [])
   const data = useMemo(() => products, [products])
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
-    useTable(
-      {
-        columns,
-        data,
-      },
-      useSortBy
-    )
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    page,
+    gotoPage,
+    nextPage,
+    previousPage,
+    prepareRow,
+    pageCount,
+    pageOptions,
+    setPageSize,
+    state: { pageIndex, pageSize },
+  } = useTable(
+    {
+      columns,
+      data,
+      initialState: { pageIndex: 1 },
+    },
+    useSortBy,
+    usePagination
+  )
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -88,7 +118,7 @@ const FetchingTable = () => {
     }
     fetchProducts()
   }, [])
-  console.log(products)
+  // console.log(products)
 
   if (products.length === 0) {
     return (
@@ -120,7 +150,7 @@ const FetchingTable = () => {
           ))}
         </Thead>
         <Tbody {...getTableBodyProps()}>
-          {rows.map((row, i) => {
+          {page.map((row, i) => {
             prepareRow(row)
             return (
               <Tr {...row.getRowProps()}>
@@ -132,6 +162,82 @@ const FetchingTable = () => {
           })}
         </Tbody>
       </Table>
+      <Flex justify="space-between" align="center">
+        <Flex gap="4">
+          <Tooltip label="First Page">
+            <IconButton
+              onClick={() => gotoPage(0)}
+              icon={<ArrowLeftIcon h="3" w="3" />}
+            ></IconButton>
+          </Tooltip>
+          <Tooltip label="Prev Page">
+            <IconButton
+              onClick={previousPage}
+              icon={<ChevronLeftIcon h="4" w="4" />}
+            ></IconButton>
+          </Tooltip>
+        </Flex>
+
+        <Flex align="center" gap={4}>
+          <Flex align="center" gap="2px">
+            Page
+            <Text fontWeight="bold" as="span">
+              {pageIndex + 1}
+            </Text>
+            of
+            <Text fontWeight="bold" as="span">
+              {pageOptions.length}
+            </Text>
+          </Flex>
+          <NumberInput
+            w={28}
+            min={1}
+            max={pageOptions.length}
+            onChange={(value) => {
+              const page = value ? value - 1 : 0
+              gotoPage(page)
+            }}
+            defaultValue={pageIndex + 1}
+          >
+            <NumberInputField />
+            <NumberInputStepper>
+              <NumberIncrementStepper />
+              <NumberDecrementStepper />
+            </NumberInputStepper>
+          </NumberInput>
+
+          <Select
+            w="32"
+            value={pageSize}
+            onChange={(e) => {
+              setPageSize(Number(e.target.value))
+            }}
+          >
+            {[5, 10, 15, 20].map((pageSize) => (
+              <option key={pageSize} value={pageSize}>
+                Show {pageSize}
+              </option>
+            ))}
+          </Select>
+        </Flex>
+
+        <Flex gap="4">
+          <Tooltip label="Next Page">
+            <IconButton
+              onClick={nextPage}
+              icon={<ChevronRightIcon h="4" w="4" />}
+            ></IconButton>
+          </Tooltip>
+          <Tooltip label="Last Page">
+            <IconButton
+              onClick={() => {
+                gotoPage(pageCount - 1)
+              }}
+              icon={<ArrowRightIcon h="3" w="3" />}
+            ></IconButton>
+          </Tooltip>
+        </Flex>
+      </Flex>
     </>
   )
 }
